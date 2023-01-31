1a0fdd0e85d76e8dbc7e45ee271cfc7d4378620d
manzeelanepali <nepalimanzeela@gmail.com>
2023-01-31 10:46:09 +0545

    feat : updated the app.js file where the functionality of the couter was added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
 
-function App() {
-  return (
-   <div>hello</div>
-  );
-}
+import { useState } from 'react'
+const App = (props) => {
+  const [counter, setCounter] = useState(0)
 
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(counter - 1)}>
+        minus
+      </button>      
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
+  )
+}
 export default App;
